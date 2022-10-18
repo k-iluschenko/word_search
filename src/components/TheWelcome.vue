ffbcf23113638aa98af2320249aa09449cba5f0f
kirill Iluschenko <kirill.i@nekidaem.ru>
2022-10-18 03:35:55 +0300

    added project logic

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,86 +1,256 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="@/assets/game.png" alt="example" height="200" />
+      <img src="@/assets/example_2.jpg" alt="example" height="200" />
+      <p>
+        Предназначено для помощи в игре "угадай слово". Указываешь какой длины слово,
+        какие буквы есть, а каких нет. Так же можно указать на каких местах были найдены
+        буквы.
+      </p>
+    </div>
+
+    <div class="findWords">
+      <label>
+        <span>Длина слова</span>
+        <input v-model="length" />
+      </label>
+    </div>
+    <div class="findWords">
+      <label>
+        <span>Буквы которых нет в слове</span>
+        <input v-model="negativeLetters" />
+      </label>
+    </div>
+    <div class="findWords">
+      <label>
+        <span>Слово содержит буквы</span>
+        <input v-model="positiveLetters" />
+      </label>
+      <p>* чаще всего встречаются буквы о, е, а, и, н, т, с, р, в, л</p>
+      <p>* буквы можно вводить через пробел, запятую или слитно</p>
+    </div>
+
+    <div class="signUpCode">
+      <p>Указываем букву которую угадали</p>
+      <template v-for="(k, i) in positiveInputs" :key="i">
+        <input
+          v-model="positiveInputs[i]"
+          :maxlength="1"
+          class="title"
+          :id="`code-${i}`"
+          :placeholder="i + 1"
+          @input="addChart(positiveInputs[i])"
+        />
+      </template>
+    </div>
+    <div class="signUpCode excludeCharts">
+      <p>Буквы есть, но они должны стоять на другом месте</p>
+      <template v-for="(k, i) in negativeInputs" :key="i">
+        <input
+          v-model="negativeInputs[i]"
+          :maxlength="length - 1"
+          class="title"
+          :id="`code-${i}`"
+          :placeholder="i + 1"
+          @input="addChart(negativeInputs[i])"
+        />
+      </template>
+    </div>
+    <div class="button_container">
+      <button @click="findWords()">Подобрать слова</button>
+      <button @click="newWord()">Новое слово</button>
+    </div>
+    <div>
+      <a
+        href="https://ru.wikipedia.org/wiki/%D0%A7%D0%B0%D1%81%D1%82%D0%BE%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C"
+        target="_blank"
+        >Частотность букв русского языка</a
+      >
+    </div>
+    <section>
+      <words-found-component :wordsList="wordsList" />
+    </section>
+  </div>
+</template>
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+  import words from '@/data/words.json'
+  import { Ref, ref, watch } from 'vue'
+  import WordsFoundComponent from './WordsFoundComponent.vue'
+
+  const length = ref<number>(6)
+  const wordsList = ref<string[]>([])
+  const negativeLetters = ref<string>('')
+  const positiveLetters = ref<string>('')
+  const regexSeparator: RegExp = /[а-я]/g
+  const positiveInputs = ref<string[]>(new Array<string>(length.value).fill(''))
+  const negativeInputs = ref<string[]>(new Array<string>(length.value).fill(''))
+
+  // Сбрасываем значения на дефолтные
+  const newWord = () => {
+    length.value = 6
+    wordsList.value = []
+    negativeLetters.value = ''
+    positiveLetters.value = ''
+    positiveInputs.value = new Array<string>(length.value).fill('')
+    negativeInputs.value = new Array<string>(length.value).fill('')
+  }
+
+  watch(length, (newLength) => {
+    positiveInputs.value = new Array<string>(+newLength).fill('')
+    negativeInputs.value = new Array<string>(+newLength).fill('')
+  })
+
+  // Добавляем символы в positive input
+  const addChart = (letter: string) => {
+    const letters = positiveLetters.value + letter
+    const uniqueChart = new Set(letters)
+    positiveLetters.value = Array.from(uniqueChart).join('')
+  }
+  // Конвертируем строку в regexp
+  const convertStringToRegexp = (
+    str: string,
+    separate: string,
+    beforeWrap: string,
+    afterWrap: string
+  ): RegExp => {
+    const newStr = str.toLowerCase().match(regexSeparator)?.join(separate) || ''
+    return new RegExp(beforeWrap + newStr + afterWrap, 'i')
+  }
+
+  // Фильтруем по длине
+  const byLength = () => {
+    return words.filter((w: string) => w.length === +length.value)
+  }
+
+  // Фильтруем по известным буквам
+  const byLetters = (
+    letters: Ref<string>,
+    type: 'positive' | 'negative'
+  ): string[] | [] => {
+    if (!letters.value) return wordsList.value
+    let regex: RegExp
+    if (type === 'positive') {
+      regex = convertStringToRegexp(letters.value, ')(?=.*', '(?=.*', ')')
+    }
+    if (type === 'negative') {
+      regex = convertStringToRegexp(letters.value, '|', '', '')
+    }
+    return wordsList.value.filter((w: string) => {
+      if (type === 'positive') return regex.test(w)
+      return !regex.test(w)
+    })
+  }
+
+  // Фильтруем по позиции буквы
+  const byPosition = (lettersList: Ref<string[]>, type: 'positive' | 'negative') => {
+    if (!lettersList.value.join('')) return wordsList.value
+    let fword = [...wordsList.value]
+    lettersList.value.forEach((c, i) => {
+      fword = fword.filter((w) => {
+        return type === 'positive' ? !c || w[i] === c : !c || !c.includes(w[i])
+      })
+    })
+    return fword
+  }
+
+  // Ищем слова
+  const findWords = () => {
+    wordsList.value = byLength()
+    wordsList.value = byLetters(negativeLetters, 'negative')
+    wordsList.value = byLetters(positiveLetters, 'positive')
+    wordsList.value = byPosition(positiveInputs, 'positive')
+    wordsList.value = byPosition(negativeInputs, 'negative')
+  }
 </script>
 
-<template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
+<style scoped lang="scss">
+  .container {
+    width: 400px;
+    min-height: 100vh;
+    align-items: center;
+    //display: flex;
+  }
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
+  input {
+    border-radius: 5px;
+    border: 1px solid #2c2a2a;
+    outline: none;
+    background-color: #f3f1f1;
+    padding-left: 12px;
+  }
 
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
+  .signUpCode {
+    margin-top: 20px;
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
+    & > input {
+      margin-top: 10px;
+      height: 44px;
+      width: 44px;
+      margin-right: 14px;
+      font-size: 36px;
+      padding-left: 12px;
+      padding-bottom: 6px;
+      place-items: center;
+      text-transform: lowercase;
 
-    <br />
+      &::placeholder {
+        color: #8c8a8a;
+        font-size: 9px;
+        transform: translate(20px, -20px);
+      }
+    }
 
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
+    &.excludeCharts {
+      & > input {
+        font-size: 10px;
 
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
+        &::placeholder {
+          transform: translate(20px, -10px);
+        }
+      }
+    }
+  }
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
+  .findWords {
+    margin-top: 10px;
 
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
+    & label {
+      display: flex;
+      width: 400px;
+    }
 
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
+    & span {
+      flex: 1;
+    }
 
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
+    & input {
+      flex: 1;
+      height: 39px;
+    }
 
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
-</template>
+    & p {
+      color: #9f9d9d;
+      font-size: 14px;
+    }
+  }
+
+  .button_container {
+    display: flex;
+
+    & button {
+      width: 100%;
+      margin-top: 10px;
+      height: 39px;
+      place-items: center;
+      background-color: #7ee0ac;
+      border: 1px solid #4a8365;
+      border-radius: 5px;
+      margin-left: 10px;
+
+      &:first-child {
+        margin-left: 0;
+      }
+    }
+  }
+</style>
