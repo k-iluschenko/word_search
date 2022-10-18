d5d3c16d79e9f34e85c61b3b47a57d020ccf7373
kirill Iluschenko <kirill.i@nekidaem.ru>
2022-10-18 03:24:37 +0300

    added the found words component

---
+++ src/components/WordsFoundComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2 v-if="!wordsList.length">Слова не найдены.</h2>
+  <h2 v-else>Найдено слов: {{ wordsList.length }}</h2>
+  <p v-if="wordsList.length">{{ wordsList }}</p>
+</template>
+<script setup lang="ts">
+  defineProps<{
+    wordsList: string[]
+  }>()
+</script>
