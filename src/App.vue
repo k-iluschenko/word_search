6378bbb9bfb94539837994bd917801c7c09615f6
kirill Iluschenko <kirill.i@nekidaem.ru>
2022-10-18 03:30:08 +0300

    removed unused files, improved app component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <title-component title="Поиск слов по буквам" />
     </div>
   </header>
-
   <main>
-    <TheWelcome />
+    <the-welcome />
   </main>
 </template>
 
+<script setup lang="ts">
+  import TheWelcome from '@/components/TheWelcome.vue'
+  import TitleComponent from '@/components/TitleComponent.vue'
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    line-height: 1.5;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  @media (min-width: 1024px) {
+    header {
+      display: flex;
+      place-items: center;
+      padding-right: calc(var(--section-gap) / 2);
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    header .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
+    }
   }
-}
 </style>
